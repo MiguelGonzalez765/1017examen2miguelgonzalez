ea5c5d0df48a45c460ec05a066becd67d41b9417
MiguelGonzalez765 <midas765@yahoo.com>
2021-02-13 18:44:48 -0500

    mes nouveaux composants

---
+++ src/views/Projet.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+          <ul>
+          <li v-for="(projet, i) in listeProjets" :key="i">
+            {{ projet.nom}}: {{ projet.description}}
+          </li>
+          </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Projet",
+  data() {
+    return {
+      listeProjets: [
+        { nom: "SpotiQuebec", langage:"REACT"},
+        { nom: "Clinique Vétérinaire",langage: " PHP"},
+        { nom: "Agence de voyages", langage: "Angular"},
+         { nom: "Celda", langage: "Ccs"},
+         { nom: "Photociti", langage: "Javascript"},
+         { nom: "Gestionnaire de nouvelles", langage: "Php"},
+
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+li{
+  padding: 5px;
+  margin: 2px;
+  list-style-type: none;
+}
+</style>
